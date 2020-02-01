79d8d0b5916335c2b7e9340f463c9b741963a051
Wilhelm Roscher <wilhelm.roscher01@gmail.com>
2020-02-01 01:36:47 +0100

    Wind Angriff

--- GGJ2020/Assets/Angriffe.cs
+++ GGJ2020/Assets/Angriffe.cs
@@ -6,6 +6,7 @@ public class Angriffe : MonoBehaviour
 {
     public float Geschwindigeit_Typ1;
     public Vector3 Anfangspos_Typ1;
+    public float stärkeWindstoß;
     public GameObject feind1_prefab;
     
     void Update()
@@ -14,7 +15,12 @@ public class Angriffe : MonoBehaviour
         {
             this.Angriff_Typ1();
         }
-        
+
+        if (Input.GetKeyDown("w"))
+        {
+            this.Angriff_Wind();
+        }
+
     }
 
     void Angriff_Typ1()
@@ -23,14 +29,28 @@ public class Angriffe : MonoBehaviour
         GameObject feind = Instantiate(feind1_prefab, Anfangspos_Typ1, Quaternion.identity);
 
         //Feind fliegt los
-        feind.GetComponent<Rigidbody2D>().AddForce(new Vector2(Geschwindigeit_Typ1 * Time.deltaTime, 0));
-        feind.transform.GetChild(0).GetComponent<Rigidbody2D>().AddForce(new Vector2(Geschwindigeit_Typ1 * Time.deltaTime, 0));
+        feind.GetComponent<Rigidbody2D>().AddForce(new Vector2(Geschwindigeit_Typ1, 0));
+        feind.transform.GetChild(0).GetComponent<Rigidbody2D>().AddForce(new Vector2(Geschwindigeit_Typ1, 0));
 
         // Feind greift an
         this.Warte(1000);
         feind.transform.GetChild(0).GetComponent<Rigidbody2D>().gravityScale = 1;
 
         //Feind fliegt weg
+    }
+
+    void Angriff_Wind()
+    {
+        GameObject[] blöcke = GameObject.FindGameObjectsWithTag("Block");
+
+        foreach (GameObject block in blöcke)
+        {
+            if (Random.value < 0.2)
+            {
+                Debug.Log("Wind");
+                block.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.value* stärkeWindstoß, Random.value* stärkeWindstoß));
+            }
+        }
     }
 
     IEnumerator Warte(int sekunden)
