60959ca425a5b444e510e567beb63fe0796c4bd0
j0k3r <christoph611@web.de>
2020-02-01 01:02:39 +0100

    CreateBlocks
    Bauen

---
+++ GGJ2020/Assets/Scripts/Create_Block.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Create_Block : MonoBehaviour
+{
+    public List<GameObject> prefabs_bloecke;
+    public Transform parent;
+    System.Random rnd = new System.Random();
+
+    List<GameObject> created = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            int random = rnd.Next(0, prefabs_bloecke.Count);
+            GameObject o = Instantiate(prefabs_bloecke[random], parent);
+            created.Add(o);
+            created[created.Count-1].AddComponent<blockcheck>();
+            Debug.Log(random);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            foreach (GameObject l in created)
+            {
+                Destroy(l);
+            }
+        }
+    }
+}
