0bcfa138df01013ad8ee2776cc26f844ecc2aa3a
Wilhelm Roscher <wilhelm.roscher01@gmail.com>
2020-02-01 10:54:27 +0100

    Reset mit r

---
+++ GGJ2020/Assets/reset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reset : MonoBehaviour
+{
+    public char reset_button;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(reset_button.ToString()))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
