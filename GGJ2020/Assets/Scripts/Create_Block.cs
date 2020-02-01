b014491c7fd49b4ef38e0cc38b5647609269ebe7
j0k3r <christoph611@web.de>
2020-02-01 13:23:52 +0100

    Slinks+ Plattformlayer

--- GGJ2020/Assets/Scripts/Create_Block.cs
+++ GGJ2020/Assets/Scripts/Create_Block.cs
@@ -24,11 +24,31 @@ public class Create_Block : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P))
         {
             int random = rnd.Next(0, prefabs_bloecke.Count);
+            int drehung = rnd.Next(0, 4);
             GameObject o = Instantiate(prefabs_bloecke[random], parent);
+
+            if (drehung == 0)
+            {
+                o.transform.eulerAngles = new Vector3(o.transform.eulerAngles.x,o.transform.eulerAngles.x,transform.eulerAngles.z + 90);
+            }
+            if (drehung == 1)
+            {
+                o.transform.eulerAngles = new Vector3(o.transform.eulerAngles.x, o.transform.eulerAngles.x, transform.eulerAngles.z - 90);
+            }
+            if (drehung == 1)
+            {
+                o.transform.eulerAngles = new Vector3(o.transform.eulerAngles.x, o.transform.eulerAngles.x, transform.eulerAngles.z + 180);
+            }
+            if (drehung == 1)
+            {
+                o.transform.eulerAngles = new Vector3(o.transform.eulerAngles.x, o.transform.eulerAngles.x, transform.eulerAngles.z);
+            }
 
             GameObject k = Instantiate(prefab_plattform, o.transform);
 
             
+
+
             k.transform.localPosition = new Vector3(UnityEngine.Random.Range(-0.25f,0.25f), UnityEngine.Random.Range(-0.25f, 0.25f), k.transform.localPosition.z);
             
 
@@ -42,12 +62,12 @@ public class Create_Block : MonoBehaviour
 
         
 
-        //if (Input.GetKeyDown(KeyCode.D))
-        //{
-        //    foreach (GameObject l in created)
-        //    {
-        //        //Destroy(l);
-        //    }
-        //}
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            foreach (GameObject l in created)
+            {
+                Destroy(l);
+            }
+        }
     }
 }
